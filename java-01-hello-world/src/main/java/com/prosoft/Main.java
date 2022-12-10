3039913986603a8b12723f340223291e8fa3063e
sproshchaev <sproshchaev@gmail.com>
2022-12-10 21:03:20 +0500

    Added java-01-hello-world

---
+++ java-01-hello-world/src/main/java/com/prosoft/Main.java
@@ -0,0 +1,14 @@
+package com.prosoft;
+
+/**
+ * Первая программа на Java
+ *
+ */
+public class Main
+{
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+
+    }
+}
