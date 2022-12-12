ba43bb3cd910b1116b168eecb50a87b73d355d57
sproshchaev <sproshchaev@gmail.com>
2022-12-12 12:37:59 +0500

    Updated

--- java-05-conditions/src/main/java/com/prosoft/Main.java
+++ java-05-conditions/src/main/java/com/prosoft/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         /**
-         * слайд 39. Условный оператор if
+         * Слайд 39. Условный оператор if
          */
         int i = 100;
         if (i <= 100) {
@@ -32,8 +32,8 @@ public class Main {
             case 1:
                 monthString = "January";
                 break; // (не обязательный) если встречается конструкция break — то управление передается за пределы команды switch.
-                       // Если его нет – код продолжит выполнение (игнорируя дальнейшие сравнения значений в блоках case)
-                       // до первого встреченного break или до конца оператора switch
+            // Если его нет – код продолжит выполнение (игнорируя дальнейшие сравнения значений в блоках case)
+            // до первого встреченного break или до конца оператора switch
             case 2:
                 monthString = "February";
                 break;
@@ -46,7 +46,65 @@ public class Main {
         /**
          * Слайд 42. Цикл while
          */
+        System.out.println("Цикл while:");
+        int count = 0;
+        while (count < 5) {
+            System.out.println("count=" + count);
+            count++;
+        }
 
+        /**
+         * Слайд 43. Цикл do-while
+         */
+        System.out.println("Цикл do-while:");
+        count = 0;
+        do {
+            System.out.println("count=" + count);
+            count++;
+        } while (count < 5);
+
+        /**
+         * Слайд 44. Цикл for
+         */
+        System.out.println("Цикл for:");
+        for (int j = 0; j < 5; j++) {
+            System.out.println("j=" + j);
+        }
+
+        /**
+         * Слайд 45. Цикл for-each
+         */
+        System.out.println("Цикл for-each:");
+        int [] nunbers = new int[] {0, 1, 2, 3, 4};
+        for (int number : nunbers) {
+            System.out.println("number=" + number);
+        }
+
+        /**
+         * Слайд 46. Оператор break
+         */
+        System.out.println("Оператор break:");
+        count = 0;
+        while (count < 5) {
+            count++;
+            if (count > 2) {
+                break; // прерывает выполнение цикла и передает управление в конец тела цикла
+            }
+            System.out.println("count=" + count);
+        }
+
+        /**
+         * Слайд 47. Оператор continue
+         */
+        System.out.println("Оператор continue:");
+        count = 0;
+        while (count < 5) {
+            count++;
+            if (count == 3) {
+                continue; // прерывает выполнения тела цикла и передает управление в начало цикла. Происходит пропуск текущей итерации цикла
+            }
+            System.out.println("count=" + count);
+        }
 
     }
 }
