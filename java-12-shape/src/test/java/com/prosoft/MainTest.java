9023fe4432648aee62b5ed0ceb433cfbc0011394
sproshchaev <sproshchaev@gmail.com>
2022-12-27 11:43:30 +0500

    Updated

--- java-12-shape/src/test/java/com/prosoft/MainTest.java
+++ java-12-shape/src/test/java/com/prosoft/MainTest.java
@@ -7,32 +7,28 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple Main.
  */
-public class AppTest 
-    extends TestCase
-{
+public class MainTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public MainTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(MainTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
