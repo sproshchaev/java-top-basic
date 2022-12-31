0930b0b4667c925949894b3449e37962515d7c2c
sproshchaev <sproshchaev@gmail.com>
2022-12-31 17:28:03 +0500

    Added java-14-generics

---
+++ java-14-generics/src/main/java/com/prosoft/Main.java
@@ -0,0 +1,11 @@
+package com.prosoft;
+
+/**
+ * java-14-generics
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
