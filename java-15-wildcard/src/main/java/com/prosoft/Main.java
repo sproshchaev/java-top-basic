51f863db2e974d66a5d7f06eccf1a3742f329e5d
sproshchaev <sproshchaev@gmail.com>
2023-01-03 21:16:22 +0500

    Added java-15-wildcard

---
+++ java-15-wildcard/src/main/java/com/prosoft/Main.java
@@ -0,0 +1,13 @@
+package com.prosoft;
+
+/**
+ * java-15-wildcard
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println( "Hello World!" );
+
+    }
+}
