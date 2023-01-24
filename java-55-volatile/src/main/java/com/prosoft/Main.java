4fa95a44c6526960131298cd2ffe092f1c681f89
sproshchaev <sproshchaev@gmail.com>
2023-01-24 17:16:05 +0500

    Added java-55-volatile

---
+++ java-55-volatile/src/main/java/com/prosoft/Main.java
@@ -0,0 +1,13 @@
+package com.prosoft;
+
+/**
+ * java-55-volatile
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println( "Hello World!" );
+
+    }
+}
