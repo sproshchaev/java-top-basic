5c6cf181c537bdae9e7fbb9c6c184f2c73d549ca
sproshchaev <sproshchaev@gmail.com>
2023-01-25 12:25:18 +0500

    Added java-56-inetaddress

---
+++ java-56-inetaddress/src/main/java/com/prosoft/Main.java
@@ -0,0 +1,13 @@
+package com.prosoft;
+
+/**
+ * java-56-inetaddress
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
