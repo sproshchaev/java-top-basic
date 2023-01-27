d312bead8d29fdcda252263b5e2f5198309a11c6
sproshchaev <sproshchaev@gmail.com>
2023-01-27 16:02:23 +0500

    Added java-59-junit-calculator

---
+++ java-59-junit-calculator/src/test/java/com/prosoft/CalculatorTest.java
@@ -0,0 +1,22 @@
+package com.prosoft;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sub() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mult() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void div() {
+    }
+}
