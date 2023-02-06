3b8386d40e675a55ad89f8c22b6761451379f5f1
sproshchaev <sproshchaev@gmail.com>
2023-02-06 15:45:20 +0500

    Updated

--- java-60-junit-exception/src/test/java/com/prosoft/CalculatorTest.java
+++ java-60-junit-exception/src/test/java/com/prosoft/CalculatorTest.java
@@ -1,11 +1,13 @@
 package com.prosoft;
 
-import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CalculatorTest extends TestCase {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class CalculatorTest {
     private Calculator calc;
 
     /**
@@ -26,22 +28,49 @@ public class CalculatorTest extends TestCase {
         this.calc = null; // удаление объекта после теста
     }
 
+    /**
+     * Положительный сценарий работы метода div2()
+     */
     @Test
     public void testDiv() throws DivByZeroException {
-        double result = calc.div(8, 2);
+        double result = calc.div2(8, 2);
         assertEquals(4, result, 1e-9);
 
     }
 
     /**
-     * Для проверки выброса исключения необходимо использовать аннотацию @Test с параметром expected
-     * Тест пройдет если исключение возникнет
+     * Тест будет пройден только в том случае, если исключение возникнет
+     * <p>
+     * Junit4 - import static org.junit.Assert.*;
      *
      * @throws DivByZeroException
      */
     @Test(expected = DivByZeroException.class)
     public void testDivByZero() throws DivByZeroException {
-        double result = calc.div(8, 0);
+        double result = calc.div2(8, 0);
+    }
+
+    /**
+     * Метод testDivByZeroMessage() выполняет проверку вызова исключения и проверку самого сообщения
+     *
+     * Если исключение не выбрасывается тестируемым методом, то тест заваливается методом fail() с сообщением о том, что
+     * должно быть исключение DivByZeroException.
+     * Если же это исключение возникает, то оно отлавливается в первом блоке catch, и методом assertEquals()
+     * выполняется сравнение сообщения, инкапсулированного в объекте-исключении, с текстом «Division by Zero». Тест
+     * будет завален, если совпадения нет.
+     * Если будет выброшено исключение другого класса, то это также приводит к неудаче теста с соответствующим выводом
+     * о несовпадении полученного исключения с ожидаемым.
+     */
+    @Test
+    public void testDivByZeroMessage() {
+        try {
+            double result = calc.div2(8, 0);
+            fail("Division by Zero should have thrown a DivByZeroException");
+        } catch (DivByZeroException e) {
+            assertEquals("Division by Zero", e.getMessage());
+        } catch (Exception e) {
+            fail("Should be DivByZeroException, but have " + e.getClass());
+        }
     }
 
 }
