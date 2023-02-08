5cc16a9d78f5672128af745a37cb62300ac39eff
sproshchaev <sproshchaev@gmail.com>
2023-02-08 10:51:10 +0500

    Updated

--- java-65-stub/src/test/java/com/prosoft/data/TodoServiceStub.java
+++ java-65-stub/src/test/java/com/prosoft/data/TodoServiceStub.java
@@ -6,6 +6,6 @@ import java.util.List;
 public class TodoServiceStub implements TodoService {
     @Override
     public List<String> retrieveTodos(String user) {
-        return Arrays.asList("Learn Spring MVC","Learn Spring","Learn to Dance");
+        return Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
     }
 }
