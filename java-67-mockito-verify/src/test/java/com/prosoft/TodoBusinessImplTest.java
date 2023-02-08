c8c7768e069f29346f071e53ad5d332bfe972653
sproshchaev <sproshchaev@gmail.com>
2023-02-08 16:41:23 +0500

    Updated

--- java-67-mockito-verify/src/test/java/com/prosoft/TodoBusinessImplTest.java
+++ java-67-mockito-verify/src/test/java/com/prosoft/TodoBusinessImplTest.java
@@ -14,27 +14,6 @@ import static org.mockito.Mockito.*;
  * Класс TodoBusinessImplTest содержит методы тестирования класса TodoBusinessImpl
  */
 public class TodoBusinessImplTest {
-    @Test
-    public void testWithMock() {
-
-        /**
-         * Создание мока и описание его поведения через
-         */
-        TodoService todoServiceMock = mock(TodoService.class);
-        List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
-        when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
-
-        /**
-         * Создание тестируемого объекта и передача мока через конструктор (внедрение зависимости)
-         */
-        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
-        List filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
-
-        /**
-         * Проверка, что в отфильтрованном списке два элемента
-         */
-        assertEquals(2, filteredTodos.size());
-    }
 
     /**
      * Метод testWithDeleteTodos() выполняет тестирование метода deleteTodosNotRelatedToSpring() в классе TodoBusinessImpl
