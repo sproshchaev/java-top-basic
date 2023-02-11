5656a385dded406bdbdc4a7e39bb5d0641499314
sproshchaev <sproshchaev@gmail.com>
2023-02-11 15:32:54 +0500

    Updated

--- java-69-spring-context/src/main/java/com/prosoft/domain/Train.java
+++ java-69-spring-context/src/main/java/com/prosoft/domain/Train.java
@@ -1,4 +1,28 @@
 package com.prosoft.domain;
 
+import org.springframework.stereotype.Component;
+
+/**
+ * (8) Класс Train
+ * Экземпляры класса добавляется в контекст через стереотипную аннотацию @Component и @ComponentScan.
+ * Работает с версией Java не ниже 11
+ * Для использования аннотации @PostConstruct необходимо в pom.xml добавить зависимость:
+ *
+ * <dependency>
+ * <groupId>Javax.annotation</groupId>
+ * <artifactId>Javax.annotation-api</artifactId>
+ * <version>1.3.2</version>
+ * </dependency>
+ */
+@Component
 public class Train {
+    private long id;
+    private String name;
+
+    public Train() {
+    }
+
+    public Train(String name) {
+        this.name = name;
+    }
 }
