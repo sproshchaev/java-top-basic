0cc7a2841eafd11da6189f5004a4c4e341ffa86d
sproshchaev <sproshchaev@gmail.com>
2023-02-13 17:37:35 +0500

    Added java-70-spring-singleton-prototype

---
+++ java-70-spring-singleton-prototype/src/main/java/com/prosoft/Main.java
@@ -0,0 +1,11 @@
+package com.prosoft;
+
+/**
+ * java-70-spring-singleton-prototype
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("java-70-spring-singleton-prototype");
+    }
+}
