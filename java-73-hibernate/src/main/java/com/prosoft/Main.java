0305c7e88330d0dc7bbabcf5efbbf1acb6deb4bb
sproshchaev <sproshchaev@gmail.com>
2023-02-16 10:44:20 +0500

    Added java-73-hibernate

---
+++ java-73-hibernate/src/main/java/com/prosoft/Main.java
@@ -0,0 +1,12 @@
+package com.prosoft;
+
+/**
+ * java-73-hibernate
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+
+}
