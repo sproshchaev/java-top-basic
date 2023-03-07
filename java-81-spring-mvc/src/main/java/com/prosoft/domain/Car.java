0a47a8cce037cab2e0e6d52c79cdebf21838555e
sproshchaev <sproshchaev@gmail.com>
2023-03-07 16:42:16 +0500

    Updated

--- java-81-spring-mvc/src/main/java/com/prosoft/domain/Car.java
+++ java-81-spring-mvc/src/main/java/com/prosoft/domain/Car.java
@@ -1,8 +1,8 @@
 package com.prosoft.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * Класс Car
